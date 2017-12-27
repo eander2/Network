816a34eaf68f1bb4bfbff40de589dc22d809ee2f
eander2 <anderse4@gmail.com>
2017-12-27 07:01:55 -0700

    Added docstrings and slight cleanup

--- Network.py
+++ Network.py
@@ -8,29 +8,49 @@ import pandas as pd
 random.seed()
 
 class Activation(object):
+    """
+    Activation:
+
+    Activation object; by default applies Sigmoid. TODO: activation object should have method
+    to create properly initialized weight and bias arrays for the activation type.
+    """
     def __init__(self):
         return
+
     @staticmethod
     def transform(x):
-        """Default to Selu"""
-        _lambda = 1.0507
-        alpha = 1.6732
+        """
+        transform:
+
+        :param x: input array for activation
+        :return: transformed array.
+        """
+
         return 1.0 / (1.0 + np.exp(-x))
-        # return _lambda * np.where(x >= 0.0, x, (alpha*np.exp(x) - alpha))
-        # return np.where(x > 0.0, x, 0)
 
     #@staticmethod
     def activation_derivative(self, x):
-        """Selu activation gradient"""
-        _lambda = 1.0507
-        alpha = 1.6732
+        """
+        activation_derivative:
+
+        :param x: array to apply derivative transform to
+        :return: transformed array
+
+        This method is used during backprop to calculate the derivative of the activation function
+        to the residual from the child layer.
+        """
         return self.transform(x) * (1 - self.transform(x))
-        # return _lambda*np.where(x >= 0.0, 1.0, self.transform(x) + alpha)
-        # return np.where(x > 0.0, 1.0, 0.0)
+
 
 class Selu(Activation):
     @staticmethod
     def transform(x):
+        """
+        transform:
+
+        :param x: array to transform
+        :return: transformed array
+        """
         """Default to Selu"""
         _lambda = 1.0507
         alpha = 1.6732
@@ -38,7 +58,14 @@ class Selu(Activation):
 
     #@staticmethod
     def activation_derivative(self, x):
-        """Selu activation gradient"""
+        """
+        activation_derivative:
+
+        :param x: array to transform
+        :return: transformed array
+
+        Method calculates the selu activation derivative.
+        """
         _lambda = 1.0507
         alpha = 1.6732
         return _lambda*np.where(x >= 0.0, 1.0, self.transform(x) + alpha)
@@ -64,27 +91,39 @@ class Softmax(Activation):
 class CrossEntropyCost(object):
 
     @staticmethod
-    def fn(a, y):
-        """Return the cost associated with an output ``a`` and desired output
-        ``y``.  Note that np.nan_to_num is used to ensure numerical
-        stability.  In particular, if both ``a`` and ``y`` have a 1.0
-        in the same slot, then the expression (1-y)*np.log(1-a)
-        returns nan.  The np.nan_to_num ensures that that is converted
-        to the correct value (0.0).
+    def fn(x, y):
+        """
+        fn():
+
+        :param x: test array
+        :param y: truth array
+        :return: cross entropy cost
+
+        Method calculates the cross entropy between the two arrays.
         """
         return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
 
     @staticmethod
-    def delta(z, a, y):
-        """Return the error delta from the output layer.  Note that the
-        parameter ``z`` is not used by the method.  It is included in
-        the method's parameters in order to make the interface
-        consistent with the delta method for other cost classes.
+    def delta(z, x, y):
         """
-        return (a-y)
+        delta:
+
+        :param z: Unused, needed for other cost types
+        :param x: test array
+        :param y: truth array
+        :return: error vector
+
+        Calculates the cross entropy delta for backprop.
+        """
+        return (x-y)
 
 
 class generic_layer(object):
+    """class generic_layer
+
+    This class implements the basics needed for a simple fully connected layer.
+    It includes the basic framework to include dropout and missing data handling
+    and should be inhereted by specific layer types."""
     def __init__(self, neuron_count, activation_class, **kwargs):
         self._parent = None
         self._child = None
@@ -116,15 +155,35 @@ class generic_layer(object):
             self.dropout = kwargs['dropout']
 
     def get_count(self):
+        """get_count:
+
+        Returns the neuron count of the layer."""
         return self._neuron_count
 
     def Parent(self):
+        """"Parent:
+
+        Returns the parent layer of the object.  TODO: this should be an iterable to
+        manage merge layers.
+        """
         return self._parent
 
     def Child(self):
+        """Child:
+
+        Returns the child layer of the object. TODO: this should be an iterable.
+        """
         return self._child
 
     def set_parent(self, layer):
+        """
+        set_parent:
+        :param layer: Parent layer
+        :return: None
+
+        Method sets the parent layer of the current object and defaults to allocating
+        weights and bias arrays for a fully connected layer.
+        """
         self._parent = layer
         #self.weights = np.random.normal(0.0, 1.0/np.sqrt(layer.get_count()), (self._neuron_count, layer.get_count()))
         self.weights = 0.1*np.random.randn(self._neuron_count, layer.get_count())
@@ -134,9 +193,25 @@ class generic_layer(object):
 
 
     def set_child(self, layer):
+        """
+        set_child:
+        :param layer: child layer
+        :return: None
+
+        Method sets the child layer for the current object.  Used for forward and backprop.
+        """
         self._child = layer
 
     def forward_propagation(self, x = None, is_backprop=False):
+        """
+        forward_propagation:
+
+        :param x: optional input activations
+        :param is_backprop: Boolean, true if operation is part of backprop; false if inference
+        :return: None; sets activity array only
+
+        Method performs forward propagation for the layer.
+        """
         w = self.weights
         missing_ratio = 0.0
         weight_ratio = 1.0
@@ -174,6 +249,14 @@ class generic_layer(object):
         self.activity = xp
 
     def backwards_propagation(self):
+        """
+        backwards_propagation:
+
+        :return: [matrix of weight updates, array of bias updates]
+
+        performs bacwards propagationf or the layer.  Returns updates for weights and biases.
+
+        """
         z_grad = self.activation.activation_derivative(self.z)
         delta = np.matmul(self._child.weights.transpose(), self._child.delta) * z_grad
 
@@ -191,26 +274,86 @@ class generic_layer(object):
 
 
 class InputLayer(generic_layer):
+    """
+    InputLayer:
+
+    Class for input layer, derived from generic_layer.  The primary purpose
+    of this layer is to push input array x directly to the activation array.
+    This simplifies forward and back propagation.
+    """
     def forward_propagation(self, x = None, is_backprop=False):
+        """
+        forward_propagation:
+
+        :param x: input value; only input layers receive this parameter.
+        :param is_backprop: True if during backprop, false if during inference
+        :return: None
+
+        Push input values directly to activity without transform.
+        """
+
+        # Find missing values in input and create mask for use during backprop
         self.missing_mask = np.isnan(x)
         # x[self.missing_mask] = 0
         self.activity = x
 
     def backwards_propagation(self):
+        """
+        backwars_propagation:
+
+        :return: None
+
+        InputLayer has no parameters to optimize.
+        """
+
         return
 
 
 class FullyConnectedLayer(generic_layer):
+    """
+    FullyConnectedLayer:
+
+    Class for fully connected layers.  Since generic_layer implements
+    methods for fc layer this is empty class and used for clarity.
+    """
+
     def void_function(self):
+        """
+        void_function:
+
+        :return: None
+
+        Function does nothing.
+        """
+
         return
 
 class Optimizer(object):
+    """
+    Generic Optimizer class.  In this case implements basic minibatch SGD.
+    """
     def __init__(self, learning_rate, lmbda, minibatch_size, **kwargs):
+        """
+        __init__:
+
+        :param learning_rate: Learning rate for sgd algorithm
+        :param lmbda: Unimplimented - parameter for orthogonalization operation
+        :param minibatch_size: Observations per batch
+        :param kwargs: kwargs for more sophisticated derived optimizers
+        """
         self.model = None
         self.learning_rate = learning_rate
         self.minibatch_size = minibatch_size
 
     def arrays_to_minibatch(self, X, Y):
+        """
+        arrays_to_minibatch:
+
+        :param X:
+        :param Y:
+        :return: tuple of feature batches and response batches.  Note: performs shuffle operation
+        on input data.
+        """
         zlist = list(zip(X,Y))
         np.random.shuffle(zlist)
         X, Y = zip(*zlist)
@@ -219,6 +362,17 @@ class Optimizer(object):
         return X_batches, Y_batches
 
     def optimize(self, X, Y, X_test = None, Y_test = None):
+        """
+        optimize:
+
+        :param X: training features
+        :param Y: training response
+        :param X_test: test data features
+        :param Y_test: test data response
+        :return: training_accuracy, test_accuracy
+
+        Performs one epoch of minibatch SGD optimization.
+        """
         X_batches, Y_batches = self.arrays_to_minibatch(X,Y)
         #ret = self.arrays_to_minibatch(X,Y)
         total_count = float(len(X))
@@ -283,11 +437,29 @@ class Optimizer(object):
         return training_accuracy, validation_accuracy
 
 class Sequential(object):
+    """
+    Sequential:
+
+    Model object patterned off of the Keras sequential object.
+    """
     def __init__(self):
+        """
+        __init__:
+
+        Initialize object.
+        """
         self.loss = None
         self.layers = []
 
     def addLayer(self, layer):
+        """
+        addLayer:
+
+        :param layer: Layer object to add to model.
+        :return: None
+
+        Adds layer to model layer array.
+        """
         self.layers.append(layer)
 
         if len(self.layers) > 1:
@@ -296,6 +468,15 @@ class Sequential(object):
 
 
     def predict(self, x, is_backprop=False):
+        """
+        predict:
+
+        :param x: feature vector for inference .
+        :param is_backprop: boolean, true if during backprop
+        :return: Output layer activities.
+
+        Method performs prediction for current model layers.  Output depends on network architecture.
+        """
         assert(len(self.layers) > 2)
 
         #self.layers[0].forward_propagation(x)
@@ -307,6 +488,13 @@ class Sequential(object):
         return self.layers[-1].activity
 
     def backprop(self, x,y):
+        """
+        backprop;
+
+        :param x: feature vector
+        :param y: response vector
+        :return: tuple(weight updates, bias updates)
+        """
         """Forward Pass"""
         self.predict(x, is_backprop=True)
 
@@ -332,17 +520,37 @@ class Sequential(object):
 
         return weight_update, bias_update
 
-
-    #def fit(self, X, Y,batch_size, num_epochs, optimizer, loss, **kwargs):
-
-
 def corrupt_data(data, fraction=0.1):
+    """
+    corrupt_data:
+
+    :param data: real array
+    :param fraction: fraction of data array to replace with np.NaN
+    :return: data array corrupted with np.NaN values
+
+    This method injects random np.NaN data into the feature array.  This
+    is used to test perfectly missing at random performance o the algorithm.
+    """
     mask = np.random.choice([True, False], size=np.prod(data.shape), p=[fraction, 1.0-fraction])
     mask = mask.reshape(data.shape)
     data[mask] = np.nan
     return data
 
 def corrupt_scanline(data, lines = None, fraction = 0.0, image_corrupt_prob=0.0):
+    """
+    corrupt_scanline:
+
+    :param data: image data
+    :param lines: iterable of bools; true if scanline is to be replaced with np.NaN
+    :param fraction: fraction of data to replace.
+    :param image_corrupt_prob: The probability that the function will result in a corrupted image
+    :return: modified image, corrupted lines
+
+    This method corrupts a certain proportion of the rows of the input image with np.NaN.
+    If the lines parameter is None the lines parameter is calculated based on fraction. This method is
+    intended to be a test case for missing at random data where not all of the input observations are
+    impacted.
+    """
     if lines is None:
         lines = np.random.choice([True, False], size=data.shape[0], p=[fraction, 1.0 - fraction])
 
